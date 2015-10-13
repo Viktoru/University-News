3af07c7f0303e7b388a013f77e3d06b5d63cda14
Victor P. Unda <llajta9@gmail.com>
2015-10-13 16:54:55 -0700

    Update dvunda.php

--- dvunda.php
+++ dvunda.php
@@ -19,7 +19,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with {Plugin Name}. If not, see {License URI}.
+along with UNDA. If not, see UNDA.
 */
 
 //Exit if accessed directly
