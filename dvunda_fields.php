c0e75bee58b69f1c7cb8fb717a5b2dc6ca47f706
Victor P. Unda <llajta9@gmail.com>
2015-10-15 14:09:48 -0700

    Update dvunda_fields.php

--- dvunda_fields.php
+++ dvunda_fields.php
@@ -13,40 +13,50 @@ function dvunda_add_metabox() {
 	
 add_action( 'add_meta_boxes', 'dvunda_add_custom_metabox'); 
 	
-	function dvunda_callback() {
-	?>
+	function dvunda_callback( $post ) {
+	wp_nonce_field( basename( __FILE__ ), 'dvunda_u_news_nonce' );
+	$dvunda_stored_meta = get_post_meta( $post->ID );
+	
+	 ?>
+
 	<div class="inside">
 			<div>
-				<label for="u_news">University News</label>
+				<label for="u_news">University News ID</label>
 			</div>
 		
 			<div>
-			<input type="text" name="u_news" id="u_news" value="">
+			<input type="text" name="u_news" id="u_news" size="25" value="<?php if ( ! empty ( $dvunda_stored_meta['u_news'] ) ) echo esc_attr( $dvunda_stored_meta['u_news'][0] ); ?>">
 			</div>
-	</div>
-		<div>
-        	<div>
+    		<div>
             	<label for="u_news_rss">RSS Website</label>
             </div>
-        </div>
-         
-         <div class="meta-editor"></div>       
-        
-        <?php
-		
-		$content = get_post_meta( $post->ID, 'editing_textarea', true );
-		$editor   = 'editing_textarea';
-		$settings = array(
-					'textarea_rows' => 4,
-					'media_buttons' => false,
-					);
-					
-					wp_editor( $content, $editor, $settings);
-		?>
-        </div>
-        
-        <?php
-		
+        	<div>
+            <input type="text" name="u_news_rss" id="u_news_rss" size="45" value="">
+			</div>        
+            
+            
+	</div>
+
+<?php
 	}
+	
+function dvunda_meta_save( $post_id ) {
+		$is_autosave = wp_is_post_autosave( $post_id );
+		$is_revision = wp_is_post_revision( $post_id );
+		$is_valid_nonce = ( isset( $_POST[ 'dvunda_u_news_nonce' ] ) && wp_verify_nonce( $_POST[ 'dvunda_u_news_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+	
+	if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+			return;
+	}
+	
+	if ( isset( $_POST[ 'u_news' ] ) ) {
+			update_post_meta( $post_id, 'u_news', sanitize_text_field( $_POST[ 'u_news' ] ) );
+		}
+
+}
+
+add_action( 'save_post', 'dvunda_meta_save' );
+
+
 	
 	
